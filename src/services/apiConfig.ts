b3af863c56bb785fcf3634a11e1399f93c28b706
Mauricio Ruiz <67916708+mauricioruizintellect@users.noreply.github.com>
2026-01-22 14:59:44 -0500

    api

--- src/services/apiConfig.ts
+++ src/services/apiConfig.ts
@@ -1,5 +1,5 @@
 const isProduction = window.location.href.includes('intellectsystem')
 
 export const API_URL = isProduction
-  ? 'https://apprhbackend-v0rz.onrender.com'
-  : 'https://apprhbackend-v0rz.onrender.com'
+  ? 'https://apprhbackend-v0rz.onrender.com/api'
+  : 'https://apprhbackend-v0rz.onrender.com/api'
